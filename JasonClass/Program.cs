50abf19de746426ea87eb33e86f01975afc5aa27
Эрик Дзантиев <erik.dzantiev.2002@mail.ru>
2021-05-13 09:18:09 +0300

    Добавьте файлы проекта.

---
+++ JasonClass/Program.cs
@@ -0,0 +1,60 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JasonClass
+{
+    class Programm
+    {
+        static void Main(string[] args)
+        {
+
+            /*  Subject teh1 = new Subject("технологии программирования", 72, 1);
+              Console.WriteLine(teh1);
+
+              Subject teh2 = new Subject("технологии программирования", 68, 2);
+              Console.WriteLine(teh2);
+
+              var teh1JSON = JsonConvert.SerializeObject(teh1);
+              Console.WriteLine(teh1JSON);
+
+              List<Subject> subjects = new List<Subject>();
+              subjects.Add(teh1);
+              subjects.Add(teh2);
+
+              JsonSerializer sr = new JsonSerializer();
+              using (StreamWriter file = new StreamWriter("data.json"))
+              {
+                  using (JsonWriter jw = new JsonTextWriter(file))
+                  {
+                      sr.Serialize(jw, subjects);
+                  }
+              }*/
+            List<Subject> sb = new List<Subject>();
+            //using (StreamReader sr = new StreamReader("data.json"))
+            //{
+            //    sb = JsonConvert.DeserializeObject<List<Subject>>(sr.ReadToEnd());
+            //}
+
+            //foreach (var s in sb)
+            //    Console.WriteLine(s);
+
+            Employee md = new Employee("Макаренко", "Мария", 0.5);
+            md.AddSubject(sb[0]);
+            md.AddSubject(sb[1]);
+            md.AddSubject(sb[2]);
+
+            var mdJson = JsonConvert.SerializeObject(md);
+            
+            Console.WriteLine(mdJson);
+            using (StreamWriter sw = new StreamWriter("employee.json"))
+                sw.WriteLine(mdJson);
+
+            //////////////
+        }
+    }
+}
---
+++ JasonClass/Subject.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JasonClass
+{
+    class Subject
+    {
+        public string name { get; }
+
+        public int NumberOfHourse { get; set; }
+
+        public int Semestr { get; set; }
+
+        public Subject(string name, int numberofHourse, int semestr)
+        {
+            this.name = name;
+            NumberOfHourse = numberofHourse;
+            Semestr = semestr;
+        }
+
+        public override string ToString()
+        {
+            return name + ": часов " + NumberOfHourse + "0 семестр " + Semestr;
+        }
+    }
+
+    class Employee
+    {
+        static int hourOfStavka = 900;
+        static int countOfEmployee = 0;
+        public string LastName { get; set; }
+        public string FirstName { get; set; }
+
+        public double Stavka { get; set; }
+
+        int hour;
+        public int HourOfStavka
+        {
+            set
+            {
+                if (value < Stavka * hourOfStavka)
+                    hour += value;
+                //else
+                 //   throw new Exception("слишком много часов");
+            }
+            get { return hour; }
+        }
+        List<Subject> ListSubjects = new List<Subject>();
+
+        public Employee(string lastName, string firstName, double stavka)
+        {
+            LastName = lastName;
+            FirstName = firstName;
+            Stavka = stavka;
+        }
+
+        public void AddSubject(Subject s)
+        {
+            try
+            {
+                HourOfStavka += s.NumberOfHourse;
+                ListSubjects.Add(s);
+            }
+            catch
+            {
+                Debug.WriteLine("Exception: не удалось добавитьб т.к. слишком много часов");
+            }
+       }
+    }
+}
